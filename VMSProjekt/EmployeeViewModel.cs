c4d5240b18af0c5c5732f2eee19ed1f95f2bc46e
kittikele <116081605+kittikele@users.noreply.github.com>
2026-02-03 10:39:23 +0100

    Feladat, az emplyenak legyen adószáma és leggyen egy szám, a még nem kiadott szabadnapok száma, jelenítse meg őket!

--- VMSProjekt/EmployeeViewModel.cs
+++ VMSProjekt/EmployeeViewModel.cs
@@ -3,5 +3,8 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxID { get; } = "43875629-4-38";
+        public int FreeDays { get; } = 15;
+
     }
 }
